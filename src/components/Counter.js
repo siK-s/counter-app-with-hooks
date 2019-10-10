ca899b7167be268ca81ea834ffdcc19687ecba44
Atayi <atayisika@gmail.com>
2019-10-10 15:09:41 +0200

    Ajout des boutons + et - du compteur

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,7 +3,11 @@ import React, { useState } from 'react';
 function Counter () {
     const [count, setCount] = useState(0);
     return (
-        <p>Le compteur est à : {count}</p>
+        <>
+            <p>Le compteur est à : {count}</p>
+            <button onClick={() => setCount(count + 1)}> + 1</button>
+            <button onClick={() => setCount(count - 1)}> - 1</button>
+        </>
     );
 }
 
